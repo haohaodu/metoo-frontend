51cf011594cb6d5b041be66146c4b048c765a807
haohaodu <hdu2899@gmail.com>
2021-09-20 11:42:48 -0400

    edit environment variables to default to prod

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import axios from "axios";
 import "./index.css";
 
 if (process.env.REACT_APP_DEPLOY_ENV === "dev") {
-  axios.defaults.baseURL = process.env.REACT_APP_DEV_API_URL;
-} else {
   axios.defaults.baseURL = process.env.REACT_APP_PROD_API_URL;
+} else {
+  axios.defaults.baseURL = process.env.REACT_APP_DEV_API_URL;
 }
 
 console.log("REACT_APP_DEPLOY_ENV: ", process.env.REACT_APP_DEPLOY_ENV);
